4b7d555364ab737aa36c6e7804ec12e3b652ffdb
Vlad Solokha <vsolokha@gmail.com>
2022-01-14 14:22:29 -0700

    added image of directions with css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,136 @@
 import React, {useState} from 'react';
+import directionsImage from './assets/prs-map.PNG';
 
 function App() {
-  const [openModal, setOpenModal] = useState(false);
-  
+  const [isQuoteOpen, setIsQuoteOpen] = useState(false);
+  const [error, setError] = useState(null);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [person, setPerson] = useState({ 
+    name: '',
+    email: '',
+    phone: '',
+    message: 'I was wondering about availability and rates. I need help with '
+  });
+
+  const handleQuoteOpenForm = () => {
+    setIsQuoteOpen(true);
+  }
+
+  function handleNameChange(e) {
+    setPerson({
+      ...person,
+      name: e.target.value
+    });
+  }
+
+  function handleEmailChange(e) {
+    setPerson({
+      ...person,
+      email: e.target.value
+    });
+  }
+
+  function handlePhoneChange(e) {
+    setPerson({
+      ...person,
+      phone: e.target.value
+    });
+  }
+
+  function handleMessageChange(e) {
+    setPerson({
+      ...person,
+      message: e.target.value
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      setIsSubmitted(true);
+      console.log(person);
+    } catch (err) {
+      setError(err);
+    }
+  }
   
   return (
     <div className="App">
       <div className="App-header">
         <h1>Pro Audio Services LLC</h1>
-        <h2>Electronics repair and service located in Columbus, Ohio</h2>
-        <h3>Get Directions: 657 N. James Rd. Columbus, OH 43219</h3>
+        <h2>Electronics and audio equipment repair and service</h2>
+        <h2>Specializing in the industry for 30+ years</h2>
+        <h3>Address: 657 N. James Rd. Columbus, OH 43219</h3>
+        <img 
+          className="directions-image"
+          src={directionsImage} 
+          alt="Map with pin that links to Google Maps directions">
+            <a 
+              href='https://www.google.com/maps/dir//Pro+Audio+Service+LLC/@39.9834382,-83.0593351,11.73z/data=!5m1!1e4'
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>Get Directions to Pro Audio Service LLC</span>
+            </a>
+        </img>
         <h3>Open Monday-Friday 10am-5pm</h3>
         <h3>Contact by phone - Call Alex (614) 340-3373</h3>
-        <h3><button>Get Quote</button></h3>
-        <h4>Name</h4>
-        <input type="text" />
-        <h4>Phone Number</h4>
-        <input type="phone" />
-        <h4>Email</h4>
-        <input type="email" required />
-        <h4>How can we help you?</h4>
-        <textarea rows="4" cols="50" placeholder="I was wondering about availability and rates. I need help with..." />
+        
+        <button className="get-quote" onClick={handleQuoteOpenForm}>
+          {"Get a Quote"} 
+        </button>
+        
+        {isQuoteOpen &&
+          <form className="quote-form" onSubmit={handleSubmit}>
+            <label>Name           
+              <input 
+                placeholder='Enter your name'
+                value={person.name}
+                onChange={handleNameChange} />
+            </label>
+
+            <label>Phone Number
+              <input 
+                placeholder='Enter your phone number'
+                type="phone"
+                value={person.phone}
+                onChange={handlePhoneChange} />
+            </label>
+
+            <label>Email*
+              <input 
+                placeholder='Enter your email'
+                type="email" 
+                required
+                value={person.email}
+                onChange={handleEmailChange} />
+            </label>
+
+            <label>How can we help you? 
+              <textarea 
+                rows="4" cols="20" 
+                value={person.message}
+                onChange={handleMessageChange} />
+            </label>
+
+            <button className="submit-quote" type="submit">Submit</button>
+
+            {isSubmitted &&
+              <div className="submit-success">
+                <h3>Thank you for your submission!</h3>
+                <p>We will be in touch with you shortly.</p>
+              </div>
+            }
+
+          </form>
+        }
+
+        {error !== null &&
+          <p className="Error">
+            {error.message}
+          </p>
+        }
+
       </div>
     </div>
   );
