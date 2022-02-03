e5507114b5e223aca3264a66bb4b5c0c3b6a26de
vladsolokha <vsolokha@gmail.com>
2022-02-02 22:00:00 -0700

    modified contacts section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,136 +1,40 @@
 import React, {useState} from 'react';
 
 function Contact() {
-    const [isQuoteOpen, setIsQuoteOpen] = useState(false);
-    const [error, setError] = useState(null);
-    const [isSubmitted, setIsSubmitted] = useState(false);
-    const [hoursShown, setHoursShown] = useState(false);
-    const [person, setPerson] = useState({ 
-        name: '',
-        email: '',
-        phone: '',
-        message: 'I was wondering about availability and rates. I need help with '
-    });
+  const [hoursShown, setHoursShown] = useState(false);
+  
+  return (
+      
+    <div className='contact-section'>
+      
+      {/* Phone number section */}
+      <a href='tel::(614) 340-3373'>
+        <h3>Call (614) 340-3373</h3>
+      </a>
 
-    const handleQuoteOpenForm = () => {
-        setIsQuoteOpen(true);
-    }
-
-    function handleNameChange(e) {
-        setPerson({
-        ...person,
-        name: e.target.value
-        });
-    }
-
-    function handleEmailChange(e) {
-        setPerson({
-        ...person,
-        email: e.target.value
-        });
-    }
-
-    function handlePhoneChange(e) {
-        setPerson({
-        ...person,
-        phone: e.target.value
-        });
-    }
-
-    function handleMessageChange(e) {
-        setPerson({
-        ...person,
-        message: e.target.value
-        });
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        try {
-        setIsSubmitted(true);
-        console.log(person);
-        } catch (err) {
-        setError(err);
-        }
-    }
-    
-    return (
-        <div className='contact-section'>
-        <a href='tel::(614) 340-3373'>
-          <h3>Call (614) 340-3373</h3>
-        </a>
-
-        <div className='hours-of-operation'>
-            
-            <button 
-                onMouseEnter={() => {setHoursShown(true)}}
-                onMouseLeave={() => {setHoursShown(false)}}>
-            See Hours of Operation
-            {hoursShown && (
-                <div className='hours-of-operation-shown'>
-                We are Open Monday - Friday 10 am - 5 pm
-                </div>
-            )}
-            </button>
-
-        </div>
-        
-        <button className="get-quote-button" onClick={handleQuoteOpenForm}>
-          {"Get a Quote"} 
-        </button>
-        
-        {isQuoteOpen &&
-          <form className="quote-form" onSubmit={handleSubmit}>
-            <label>Name           
-              <input 
-                placeholder='Enter your name'
-                value={person.name}
-                onChange={handleNameChange} />
-            </label>
-
-            <label>Phone Number
-              <input 
-                placeholder='Enter your phone number'
-                type="phone"
-                value={person.phone}
-                onChange={handlePhoneChange} />
-            </label>
-
-            <label>Email*
-              <input 
-                placeholder='Enter your email'
-                type="email" 
-                required
-                value={person.email}
-                onChange={handleEmailChange} />
-            </label>
-
-            <label>How can we help you? 
-              <textarea 
-                rows="4" cols="20" 
-                value={person.message}
-                onChange={handleMessageChange} />
-            </label>
-
-            <button className="submit-quote" type="submit">Submit</button>
-
-            {isSubmitted &&
-              <div className="submit-success">
-                <h3>Thank you for your submission!</h3>
-                <p>We will be in touch with you shortly.</p>
+      {/* Hours section */}
+      <div className='hours-of-operation'>
+          <button 
+              onMouseEnter={() => {setHoursShown(true)}}
+              onMouseLeave={() => {setHoursShown(false)}}>
+          See Hours of Operation
+          {hoursShown && (
+              <div className='hours-of-operation-shown'>
+              We are Open Monday - Friday 10 am - 5 pm
               </div>
-            }
-            
-            {error !== null &&
-              <p className="Error">
-                {error.message}
-              </p>
-            }
-
-          </form>
-        }
-      </div>  
-    );
+          )}
+          </button>
+      </div>
+      
+      {/* Address section */}
+      <div className='message-section'>
+        <button className="message-button">
+          Message Us
+        </button>
+      </div>
+      
+    </div>  
+  )
 }
 
 export default Contact;
--- src/components/Directions.tsx
+++ src/components/Directions.tsx
@@ -3,10 +3,20 @@ import React, {useRef, useState, useEffect} from 'react';
 
 function Directions() {
         
+    const googleMapUrl = "https://maps.googleapis.com/maps/api/staticmap?center=columbus&zoom=10&scale=2&size=300x200&maptype=roadmap&key={process.env.REACT_APP_MAPS_STATIC_API}&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C657+N+James+Rd,+Columbus,+OH+43219"
+
     return (
         <div className='directions-section'>
+            {/* Directions to Pro Audio Service */}
+            <a href="https://www.google.com/maps/dir//Pro+Audio+Service+LLC/@39.9834382,-83.0593351,11.73z/data=!5m1!1e4">
+
+                {/* Google Static Map with API */}
+                <img src={googleMapUrl} width="300"
+                alt="Map with pin that links to Google Maps directions" />
+                
             
-            
+            </a>
+          
         </div>
     );
 }
